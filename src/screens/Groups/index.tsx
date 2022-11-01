948eb27637417c6a1bd93402a1d9c76a738b326d
Diego Almeida <diegoalmda@gmail.com>
2022-11-01 20:18:41 -0300

    chore: created Button component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,16 +1,36 @@
+import { Button } from '@components/Button';
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+import { ListEmpty } from '@components/ListEmpty';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 import { 
   Container,  
 } from './styles';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Galera da Rocket', 'Amigos'])
   return (
     <Container>
-      <Header showBackButton />
+      <Header />
       <Highlight title="Turmas" subtitle="Joque com a sua turma" />
-      <GroupCard title="Galera do Ignite" />
+
+      <FlatList 
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <GroupCard 
+            title={item} 
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => <ListEmpty message="Que tal cadastrar a primeira turma?" />}
+      />
+
+      <Button 
+        title='Criar nova turma'
+      />
     </Container>
   );
 }
