5a50870ec1aa23f0e9756915983be96fb4f03c4a
Diego Almeida <diegoalmda@gmail.com>
2022-11-01 20:46:38 -0300

    chore: created new group screen

--- App.tsx
+++ App.tsx
@@ -2,8 +2,8 @@ import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import theme from './src/theme';
-import { Groups } from '@screens/Groups';
 import { Loading } from '@components/Loading';
+import { NewGroup } from '@screens/NewGroup';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -15,7 +15,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      { fontsLoaded ? <Groups /> : <Loading /> }
+      { fontsLoaded ? <NewGroup /> : <Loading /> }
     </ThemeProvider>
   );
 }
