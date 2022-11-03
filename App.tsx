39e8eff2648f33137bd4e61fa9bf93203c46a5df
Diego Almeida <diegoalmda@gmail.com>
2022-11-03 11:22:16 -0300

    chore: Created Players screen

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from 'styled-components';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import theme from './src/theme';
 import { Loading } from '@components/Loading';
-import { NewGroup } from '@screens/NewGroup';
+import { Players } from '@screens/Players';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -15,7 +15,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      { fontsLoaded ? <NewGroup /> : <Loading /> }
+      { fontsLoaded ? <Players /> : <Loading /> }
     </ThemeProvider>
   );
 }
