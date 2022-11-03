59c120c85c394c1368548ca829afe4ce5b0c8b03
Diego Almeida <diegoalmda@gmail.com>
2022-11-03 19:40:53 -0300

    chore: created remove group by name function

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -6,7 +6,8 @@ import { Highlight } from '@components/Highlight';
 import { Input } from '@components/Input';
 import { ListEmpty } from '@components/ListEmpty';
 import { PlayerCard } from '@components/PlayerCard';
-import { useRoute } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { groupRemoveByName } from '@storage/group/groupRemoveByName';
 import { playerAddByGroup } from '@storage/player/playerAddByGroup';
 import { playerRemoveByGroup } from '@storage/player/playerRemoveByGroup';
 import { playersGetByGroupAndTeam } from '@storage/player/playersGetByGroupAndTeam';
@@ -27,6 +28,7 @@ export function Players() {
   const [team, setTeam] = useState('Time A');
   const [players, setPlayers] = useState<PlayerStorageDTO[]>([]);
 
+  const navigation = useNavigation();
   const route = useRoute();
   const { group } = route.params as RouteParams;
 
@@ -81,6 +83,34 @@ export function Players() {
       console.log(error);
       Alert.alert('Remover pessoa', 'Não foi possivel remover essa pessoa.');
     }
+  }
+
+  async function groupRemove() {
+    try {
+      await groupRemoveByName(group);
+      navigation.navigate('groups');
+
+    } catch(error) {
+      console.log(error);
+      Alert.alert('Remover grupo', 'Não foi possível remover o grupo.');
+    }
+  }
+
+  async function handleGroupRemove() {
+    Alert.alert(
+      'Remover',
+      'Deseja remover o grupo?',
+      [
+        {
+          text: 'Não', 
+          style: 'cancel'
+        },
+        {
+          text: 'Sim', 
+          onPress: () => groupRemove()
+        },
+      ]
+    );
   }
 
   useEffect(() => {
@@ -152,6 +182,7 @@ export function Players() {
       <Button 
         title='Remover Turma'
         type='SECONDARY'
+        onPress={handleGroupRemove}
       />
     </Container>
   );
